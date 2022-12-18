f028043b55f7756585005285199d81c1ef11d342
Bee Nguyen <thangnh.info@gmail.com>
2022-12-18 21:20:33 +0700

    commit

--- stats/attendance.py
+++ stats/attendance.py
@@ -7,7 +7,7 @@ import tkinter
 # matplotlib.use('nbAgg')
 
 attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
-attendance.columns = ['year','attendance']
+attendance.columns = ['year', 'attendance']
 attendance.loc[:, 'attendance'] = pd.to_numeric(attendance.loc[:, 'attendance'])
 
 attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar')
--- stats/data.py
+++ stats/data.py
@@ -2,6 +2,7 @@ import os
 import glob
 import pandas as pd
 
+print(os.getcwd())
 game_files = glob.glob(os.path.join(os.getcwd(),'games','*.EVE'))
 game_files.sort()
 game_frames = []
